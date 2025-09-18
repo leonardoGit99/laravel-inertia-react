af758d489d8a4ae5aaae86fb0f51609a4c09d5e9
Leonardo Fuentes Claros <leonardofuentesclaros@gmail.com>
2025-09-17 22:01:44 -0400

    feat: add redirection home in logo

--- laravel-app/resources/js/layouts/Layout.tsx
+++ laravel-app/resources/js/layouts/Layout.tsx
@@ -1,11 +1,14 @@
 import { Link } from '@inertiajs/react';
 import React from 'react';
+import { route } from 'ziggy-js';
 
 function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="bg-gray-100 text-gray-900">
             <nav className="flex w-full justify-between bg-sky-600 p-4 px-7 text-white">
-                <h1 className="text-xl font-bold hover:cursor-pointer" /* onclick="location.href='{{ route('posts.index') }}'" */>My Blog</h1>
+                <Link href={route('posts.index')}>
+                <h1 className="text-xl font-bold hover:cursor-pointer">My Blog</h1>
+                </Link>
                 <Link href="/posts/create" className="font-semibold hover:cursor-pointer hover:underline">
                     Create post
                 </Link>
--- laravel-app/resources/js/pages/Posts/PostForm.tsx
+++ laravel-app/resources/js/pages/Posts/PostForm.tsx
@@ -1,9 +1,10 @@
 import Form from '@/components/Posts/Form';
+import Layout from '@/layouts/Layout';
 import { Post } from '@/types/Post';
 
 function PostForm({ post }: { post?: Post | null }) {
   console.log(post)
     return <Form post={post} />;
 }
-
+PostForm.layout = (page: React.ReactNode) => <Layout>{page}</Layout>;
 export default PostForm;
